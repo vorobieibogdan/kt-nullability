2fd2b954f653b3299bb64e9f81c27cba16fe43eb
Bohdan Vorobiei <vbs@ukr.net>
2025-10-07 16:37:30 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/kotlin/mate/academy/NumberTransformation.kt
    #	src/main/kotlin/mate/academy/TotalPrice.kt

--- src/main/kotlin/mate/academy/TotalPrice.kt
+++ src/main/kotlin/mate/academy/TotalPrice.kt
@@ -4,7 +4,7 @@ package mate.academy
     Calculate the products total price based on the provide price per product and products amount.
     If the price is not provided, use default price per product equal to 29.99
  */
-fun calculateTotalPrice(pricePerProduct: Double?, count: Int) : Double {
+fun calculateTotalPrice(pricePerProduct: Double?, count: Int): Double {
     var price = 29.99
     if (pricePerProduct != null) {
         price = pricePerProduct
